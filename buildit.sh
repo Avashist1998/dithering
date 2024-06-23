5e1a22e9b17aa22f58d3bfd9e2aa300a79d9d2e5
avashist1998 <abhay.vashist1998@gmail.com>
2024-06-23 13:00:30 -0500

    feat: build commit

---
+++ buildit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+git checkout main
+git pull
+
+
+echo "Building project"
+cd web-app
+wasm-pack build --target web
+
+cp -r pkg ../
+rm ../pkg/.gitignore
+cp index.html ../
+cp style.css ../
+cp index.js ../
+
+git add .
+git stash
+git checkout gh-pages -f
+git stash pop
+git add .
+git commit -m "feat: Deployment"
+git push -f
+git checkout main
