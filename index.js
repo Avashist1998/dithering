42b85dd744bdf860e4ca403d22ebfa1fb0aee02d
avashist1998 <abhay.vashist1998@gmail.com>
2024-06-24 21:53:16 -0500

    feat: Deployment

---
+++ index.js
@@ -0,0 +1,82 @@
+import init, { black_and_white, floyd_steinberg_dithering, atkinson_dithering } from './pkg/web_app.js';
+
+
+const loadingSpinner = document.getElementById('loading-spinner');
+const main = document.getElementById("main");
+function showLoading() {
+    loadingSpinner.classList.remove('hidden');
+    main.classList.add('hidden');
+}
+
+function hideLoading() {
+  loadingSpinner.classList.add('hidden');
+  main.classList.remove('hidden');
+}
+
+document.getElementById('upload').addEventListener('change', async (event) => {
+
+    showLoading();
+
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = async function(e) {
+        const img = new Image();
+        img.src = e.target.result;
+
+        img.onload = async function() {
+            const canvas = document.getElementById('input-canvas');
+            const ctx = canvas.getContext('2d');
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+
+            const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            const data = imageData.data;
+            
+            // Initialize the Rust Wasm module
+            await init();
+            // Process the image data using the Rust Wasm function
+            const binaryData = black_and_white(data, canvas.width);
+            
+            const blackWhiteCanvas = document.getElementById('b-w-canvas');
+            const bwCtx = blackWhiteCanvas.getContext('2d');
+            
+            blackWhiteCanvas.width = img.width;
+            blackWhiteCanvas.height = img.height;
+            const binaryImageData = new ImageData (new Uint8ClampedArray(binaryData), blackWhiteCanvas.width, blackWhiteCanvas.height);
+            bwCtx.putImageData(binaryImageData, 0, 0);
+            
+            
+            const FSData = floyd_steinberg_dithering(data, canvas.width);
+            console.log(FSData);
+            const FSCanvas = document.getElementById('f-s-canvas');
+            const fsCtx = FSCanvas.getContext('2d');
+            
+            FSCanvas.width = img.width;
+            FSCanvas.height = img.height;
+            const FSImageData = new ImageData (new Uint8ClampedArray(FSData), FSCanvas.width, FSCanvas.height);
+            fsCtx.putImageData(FSImageData, 0, 0);
+
+
+            const AData = atkinson_dithering(data, canvas.width);
+            console.log(AData);
+            const ACanvas = document.getElementById('a-canvas');
+            const ACtx = ACanvas.getContext('2d');
+            
+            ACanvas.width = img.width;
+            ACanvas.height = img.height;
+            const AImageData = new ImageData (new Uint8ClampedArray(AData), ACanvas.width, ACanvas.height);
+            ACtx.putImageData(AImageData, 0, 0);
+
+            const elements = document.querySelectorAll('.image-matrix');
+            elements.forEach(element => {
+                element.style.visibility = "visible";
+            })
+
+            hideLoading();
+        };
+    };
+
+    reader.readAsDataURL(file);
+});
---
+++ pkg/web_app_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function black_and_white(a: number, b: number, c: number, d: number): void;
+export function floyd_steinberg_dithering(a: number, b: number, c: number, d: number): void;
+export function atkinson_dithering(a: number, b: number, c: number, d: number): void;
+export function main_js(): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_start(): void;
